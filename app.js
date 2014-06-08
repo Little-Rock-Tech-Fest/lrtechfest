56863b72fbb6fb329dd1405ed978968393916546
Kyle Neumeier <kneumei@gmail.com>
2014-06-08 11:32:57 -0500

    updated sponsor page

--- app.js
+++ app.js
@@ -18,10 +18,6 @@ app.get("/become-sponsor", function(req, res){
 	res.render('become_sponsor')
 });
 
-app.get("/contact", function (req, res) {
-	res.render('contact')
-});
-
 app.get('/', function(req, res){
 	res.render('index')
 });
