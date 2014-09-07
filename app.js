00596fb0b7369f3dbe4ff6fbc1a6ab6ba94ccffc
Kyle Neumeier <kneumei@gmail.com>
2014-09-07 12:01:32 -0500

    added schedule

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var _ = require('lodash')._;
+var slugs = require('slugs');
 var fs = require('fs');
 var app = express();
+app.locals._ = _;
+
 
 app.set('views', __dirname+"/views");
 app.set('view engine', 'jade');
@@ -9,6 +13,33 @@ app.use(express.static(__dirname, 'public'));
 app.get("/call", function(req, res){
 	res.redirect('speakers')
 });
+
+app.get("/schedule", function(req, res){
+	fs.readFile('speakers.json', 'utf8', function(err, data){
+		if(err){
+			console.log(err);
+		}else{
+			var speakers = JSON.parse(data);
+			var talks = [];
+			_.each(speakers, function(speaker){
+				_.each(speaker.Presentations, function(presentation){
+					var foundPresentation = _.find(talks, {Topic: presentation.Topic});
+					if(foundPresentation){
+						foundPresentation.Speakers.push(speaker.FirstName + " " + speaker.LastName);
+					}else{
+						presentation.Speakers = [];
+						var speakerName = speaker.FirstName + " " + speaker.LastName;
+						presentation.Speakers.push(speakerName);
+						presentation.Url = '/speakers#'+slugs(speakerName);
+						talks.push(presentation);
+					}
+				});
+			});
+			res.render('schedule', {presentations:talks})
+		}
+	});
+});
+
 
 app.get("/sponsors", function(req, res){
 	res.render('become_sponsor');
@@ -36,6 +67,9 @@ app.get("/speakers", function (req, res) {
 			console.log(error);
 		} else {
 			var speakers = JSON.parse(data);
+			_.each(speakers, function(speaker){
+				speaker.Slug = slugs(speaker.FirstName + " " + speaker.LastName);
+			});
 			res.render('speakers', { speakers: speakers });
 		}
 	});
