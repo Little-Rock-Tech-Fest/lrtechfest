9790603818ad7f928cf4e21f20e21cd0679da7b3
Abby Sims <abby@idestini.com>
2016-05-24 15:02:53 -0500

    sponsor section logic. footer. nav updates

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var less = require('gulp-less');
 var path = require('path');
 
 gulp.task('less', function () {
-  return gulp.src(['./less/styles/style-green-color.less','./less/animations.less'])
+  return gulp.src(['./less/styles/style.less','./less/styles/style-green-color.less','./less/animations.less'])
     .pipe(less({
       paths: [ path.join(__dirname, 'less') ]
     }))
