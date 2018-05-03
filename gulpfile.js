98b840596dbeac14d059e471636895a493a81ced
Curtis Weeks <curtis@jabbtech.com>
2018-05-02 22:33:59 -0500

    publish past events

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,11 @@ var less = require('gulp-less');
 var path = require('path');
 
 gulp.task('less', function () {
-  return gulp.src(['./less/styles/style.less','./less/styles/style-green-color.less','./less/animations.less'])
-    .pipe(less({
+  return gulp.src([
+  	'./less/styles/style.less',
+  	'./less/styles/style-green-color.less',
+  	'./less/animations.less'
+  ]).pipe(less({
       paths: [ path.join(__dirname, 'less') ]
     }))
     .pipe(gulp.dest('./public/css/'));
