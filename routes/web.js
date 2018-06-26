2c0da92bc9de9c85367b2483b0e659867b29c480
Abby Sims <abby@idestini.com>
2018-06-26 11:01:02 -0500

    fix error displaying sponsors

--- routes/web.js
+++ routes/web.js
@@ -13,21 +13,16 @@ var _ = require('lodash');
 
 router.get('/', function(req, res){
 	var sponsors;
-
 	fs_readFile('sponsors.json', 'utf8')
 		.then(function(sponsorData){
 			sponsors = JSON.parse(sponsorData);
 			console.log(sponsors);
-			//res.render('sponsors', {sponsors: sponsorsDataList});
+			res.render('index', {title: 'Little Rock Tech Fest' , sponsors: sponsors});
 		})
 		.catch(function(e){
 			console.log(e);
 			res.render('500');
 		});
-
-	res.render('index', {
-		title: 'Little Rock Tech Fest' , sponsors: sponsors
-	});
 });
 
 router.get('/resources', function(req, res){
