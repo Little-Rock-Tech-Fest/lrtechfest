3ea7b5e0f63902f88cd6af6ac54507fd3df7e2a6
Curtis Weeks <curtis@jabbtech.com>
2018-09-14 00:14:56 -0500

    topics dev

--- routes/topics.js
+++ routes/topics.js
@@ -11,8 +11,15 @@ var fs_readFile = Q.denodeify(fs.readFile);
 
 var speakers = JSON.parse(fs.readFileSync('speakers.json', 'utf8'));
 var sponsors = JSON.parse(fs.readFileSync('sponsors.json', 'utf8'));
-var times = {'1-1': '9:00 AM', '1-2': '10:00 AM', '1-3': '11:00 AM', '1-4':"1:30 PM", '1-5':"2:30 PM", '1-6':"3:30 PM",
-			 '2-1': '8:30 AM', '2-2': '9:30 AM', '2-3': '10:30 AM', '2-4':"1:00 PM", '2-5':"2:00 PM", '2-6':"3:00 PM" };
+var rooms = ['1','2','3','4']
+var times = {
+	'1' : '9', 
+	'2' : '10', 
+	'3' : '11',
+	'4' : '1', 
+	'5' : '2', 
+	'6' : '3', 
+};
 var presentations = [];
 
 speakers.forEach(function(speaker){
@@ -46,6 +53,10 @@ router.get('/', function (req, res) {
 
 router.get('/staging', function (req, res) {
 	res.render('topics-staging', {speakers: speakers});
+});
+
+router.get('/dev', function (req, res) {
+	res.render('topics-dev', {speakers: speakers, rooms: rooms, times: times});
 });
 
 module.exports = router;
