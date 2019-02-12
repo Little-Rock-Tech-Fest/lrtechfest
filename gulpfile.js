6345bc88b835857651f7ea41454076fabbf759e4
Curtis Weeks <curtis@jabbtech.com>
2019-02-11 23:23:05 -0600

    reinstall gulp

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+
+gulp.task('less', function () {
+	return gulp.src([
+		'./less/styles/style.less',
+		'./less/styles/style-green-color.less',
+		'./less/animations.less'
+		]).pipe(less({
+			javascriptEnabled: true,
+			paths: [ path.join(__dirname, 'less') ]
+		}))
+		.pipe(gulp.dest('./public/css/'));
+	});
+
+gulp.task('default', ['less']);
